0fdec11cefd15b374cf3ab8c6930e1e42196d39c
unknown <gemabravosnchez@yahoo.es>
2019-05-15 17:20:33 +0200

    mainApp_hereda_de_Tk()

---
+++ 2612_mainApp_hereda_de_Tk.py
@@ -0,0 +1,39 @@
+# mainApp() hereda de Tk()
+
+from tkinter import *
+
+class mainApp():
+    
+    # assumptions:
+    
+    # 1.- screen:
+    size_screen         = '640x480'
+    window_title        = 'my_screen'
+    background_screen   = 'lightgray'
+    
+    
+    def __init__(self):
+        
+        # 1.- screen --> root (main_window)
+            # a) create
+        self.root = Tk()
+            # b) customize
+                # size
+        self.root.geometry(self.size_screen)
+                # title
+        self.root.title(self.window_title)
+                # backgroud
+        self.root.configure(bg = self.background_screen)
+
+    def maincycle(self):
+        self.root.mainloop()
+               
+
+
+
+
+
+if __name__ == '__main__':
+    app = mainApp()
+    app.maincycle()
+    
